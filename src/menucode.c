af385bcb40f9a376132370be34fd089549a034a8
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2019-11-26 15:01:22 -0700

    Prototyping

--- src/menucode.c
+++ src/menucode.c
@@ -10,12 +10,14 @@ u8 g_MenuPos = 0;
 void g_InitMainMenu()
 {
 	mmStart(MOD_THEME, MM_PLAY_LOOP); //Start playing the menu theme
-	hrt_DMA_Copy(3, m_bg_Tiles, (u32*)0x600C000, 448, 0x80000000);
-    hrt_DMA_Copy(3, m_bg_Map, (u32*)0x6002000, 1024, 0x80000000);
-    hrt_DMA_Copy(3, m_titleTiles, (u32*)0x6008000, 800, 0x80000000);
-    hrt_DMA_Copy(3, m_titleMap, (u32*)0x6002800, 1024, 0x80000000);
-    hrt_LoadBGPal((void*)m_bgPalette, 16);
+	//hrt_DMA_Copy(3, m_bg_Tiles, (u32*)0x600C000, 448, 0x80000000);
+    //hrt_DMA_Copy(3, m_bg_Map, (u32*)0x6002000, 1024, 0x80000000);
+    //hrt_DMA_Copy(3, m_titleTiles, (u32*)0x6008000, 800, 0x80000000);
+    //hrt_DMA_Copy(3, m_titleMap, (u32*)0x6002800, 1024, 0x80000000);
+	//hrt_LoadBGPal((void*)m_bgPalette, 16);
 	//memcpy(m_bg_Tiles, VRAM[0xC000], 448);
+	
+	//hrt_LoadVRAMData(data, length, offset);
     hrt_InitTiledText(0);
     hrt_BGSetMapBase(2, 4);
     hrt_BGSetTileBase(2, 3);
