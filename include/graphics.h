98036d5466314602db529377bad107de7347f24c
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2019-12-02 17:00:39 -0700

    Update

--- include/graphics.h
+++ include/graphics.h
@@ -1,5 +1,12 @@
+//Title Menu GFX
 extern const unsigned short m_bg_Map[];
 extern const unsigned char m_bg_Tiles[];
 extern const unsigned short m_titleMap[];
 extern const unsigned char m_titleTiles[];
 extern const unsigned short m_bgPalette[];
+
+//Level 1 Data
+const unsigned short level1Tiles[816];
+const unsigned short level1MetaTiles[84];
+const unsigned short level1MetaMap[3000];
+const unsigned short level1Pal[16];
