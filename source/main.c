a71b6cb7e0a6720017dddf74f33f764f0f175fe9
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2020-05-21 14:10:13 -0700

    stuff

--- source/main.c
+++ source/main.c
@@ -6,14 +6,13 @@
 int main()
 {
 	mmInitDefault((void*)soundbank_bin, 16);
-	mmStart(MOD_WORLD3, MM_PLAY_LOOP);
+	mmStart(MOD_WORLD4, MM_PLAY_LOOP);
 	hrt_InitIRQ();
 	hrt_EnableIRQ(IRQ_VBLANK);
 	hrt_SetIRQ(IRQ_VBLANK, vblFunc);
 	hrt_DSPDisableForceBlank();
 	hrt_DSPEnableBG(2);
 	hrt_DSPSetBGMode(3);
-	hrt_PlotPixelInMode3(0,0,0x001f);
 	while(1)
 	{
 		hrt_VblankIntrWait();
